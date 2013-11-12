4cd29d76dd2d85ce63d47467cea8cfa3a16982d3
macroy <macroy@macroy-PC>
2013-11-13 01:50:53 +0530

    new commit

---
+++ add numbers/src/Add.java
@@ -0,0 +1,18 @@
+
+public class Add {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = addNumbers(2,3);
+		System.out.println(a);
+	}
+
+	private static  int addNumbers(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+
+}
